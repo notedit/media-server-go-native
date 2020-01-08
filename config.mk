4b84572c5b39f08e4dfaa44f0fe9e0c0f6b73e63
notedit <notedit@gmail.com>
2020-01-08 23:36:30 +0800

    fix

--- config.mk
+++ config.mk
@@ -9,15 +9,15 @@ SANITIZE		= no
 VADWEBRTC		= yes
 SRCDIR			= ${ROOT_DIR}/media-server
 IMAGEMAGICK		= no
-PREFIX 			= /var/opt/
+PREFIX 			= /usr/local/
 
 STATIC_OPENSSL  = yes
 STATIC_LIBSRTP  = yes
 STATIC_LIBMP4   = yes
 
-OPENSSL_DIR     = /var/opt/
-LIBSRTP_DIR     = /var/opt/
-LIBMP4_DIR      = /var/opt/
+OPENSSL_DIR     = /usr/local/
+LIBSRTP_DIR     = /usr/local/
+LIBMP4_DIR      = /usr/local/
 
 
 
